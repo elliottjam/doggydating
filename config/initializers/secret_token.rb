5f79a09b05e3ef9578b2c4186bfc242b732af2f4
James Elliott <elliottjam@gmail.com>
2013-08-27 16:56:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doggydating::Application.config.secret_token = '9a2d6dcfcebe6a819e736b146b8af3de4c6471fad353a85dd25530f0338cf226105af89360fa7ec3ca8d02dcb71440ddc5bb7f65a5891a3921b1b0d56278143e'
