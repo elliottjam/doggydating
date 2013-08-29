4ab6fef95851ff598fe62515b035f49956d953e6
James Elliott <elliottjam@gmail.com>
2013-08-29 17:05:48 +0100

    routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,20 +3,18 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Doggy Dating App'" do
-      visit root_path
+    it "should have the content 'Doggy Dating App'" do
       expect(page).to have_content('Doggy Dating App')
     end
 
-it "should have the base title" do
-      visit root_path
-      expect(page)page.to have_title("Doggy Dating App")
+    it "should have the base title" do
+      expect(page).to have_content("Doggy Dating App")
     end
 
     it "should not have a custom page title" do
-      visit root_path
-      expect(page).to have_title(' | Home')
+      expect(page).not_to have_content(' | Home')
     end
   end
 
@@ -29,7 +27,7 @@ it "should have the base title" do
 
     it "should have the title 'Help'" do
       visit help_path
-      expect(page).to have_title("Doggy Dating App | Help")
+      expect(page).to have_content("Doggy Dating App | Help")
     end
   end
 
@@ -42,7 +40,7 @@ it "should have the base title" do
 
     it "should have the title 'About us'" do
        visit about_path
-      expect(page).to have_title("Doggy Dating App | About Us")
+      expect(page).to have_content("Doggy Dating App | About ")
     end
   end
 
@@ -55,7 +53,7 @@ it "should have the base title" do
 
     it "should have the title 'Contact Us'" do
       visit contact_path
-      expect(page).to have_title("Doggy Dating App | Contact")
+      expect(page).to have_content("Doggy Dating App | Contact")
     end
   end
 end
