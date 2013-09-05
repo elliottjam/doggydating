872d8867f624bac3274ddb8cc3539760d579e9f7
James Elliott <elliottjam@gmail.com>
2013-09-05 16:09:58 +0100

    fomratting container and background

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,8 @@ class Dog < ActiveRecord::Base
   belongs_to :user
   belongs_to :breed
 
+
+
   attr_accessible :age, :breed_id, :dog_image, :gender, :name, :user_id, :size, :toy, :avatar
   mount_uploader :avatar, AvatarUploader
 
